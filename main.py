56b5d88e0a90994cc3f95ede35255a10428a0c28
AmandineAndre <aa.amandine.andre@gmail.com>
2024-02-16 10:44:16 +0100

    Ajout des onglets

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
 import streamlit as st
 import pandas as pd
 import correlation
+from data_management import chargement_data
+# from data_management import nettoyage
 
-# Fonction pour charger les données
-def charger_donnees(nom_base):
-    if nom_base == 'base_diabete':
-        return pd.read_csv(r'data\diabete.csv', sep=",")
-    elif nom_base == 'base_vins':
-        return pd.read_csv(r'data\vin.csv', sep=",")
-
-# Fonction pour nettoyer et standardiser les données (simulée ici)
-def nettoyer_standardiser_donnees():
-    message = 'Les données ont été nettoyées'
-    return message
-
-# Fonction pour fractionner les données en ensembles d'entraînement et de test
-def fractionner_donnees():
-    message = 'Les données ont été splitées'
-    return message
-    
-# Fonction pour créer et entraîner le modèle (simulée ici)
-def entrainer_modele():
-    message = 'Le modèle est créé et entrainé'
-    return message
-    
-# Fonction pour évaluer le modèle sur l'ensemble de test (simulée ici)
-def evaluer_modele():
-    message = "Voici les résultats du modèle pour l'évaluer"
-    return message
-
-
-donnees = charger_donnees('base_diabete')
+donnees = chargement_data.charger('base_diabete')
 
 
 
@@ -44,33 +18,38 @@ etapes = ['Chargement des données', 'Data Management', 'Split des données', 'M
 choix_etape = st.sidebar.selectbox('Sélectionner une étape :', etapes)
 
 # Choix de l'étape 
-if choix_etape == 'Chargement des données' :
-    st.subheader('Chargement des données')
-    choix_base = st.radio('Choisir une base de données :', ('base_diabete', 'base_vins'))
-    donnees = charger_donnees(choix_base)
-    st.write('Aperçu des données :')
-    st.write(donnees.head())
+# if choix_etape == 'Chargement des données' :
+#     st.subheader('Chargement des données')
+#     choix_base = st.radio('Choisir une base de données :', ('base_diabete', 'base_vins'))
+#     donnees = chargement_data.charger(choix_base)
+#     st.write('Aperçu des données :')
+#     st.write(donnees.head())
 
+tabs_1, tabs_2, tabs_3, tabs_4, tabs_5 = st.tabs(['Statistiques descriptives', 'Nettoyage des données', 'Préparation au modèle', 'Modèle','Prédictions'])
 
-elif choix_etape == 'Data Management':
-    st.subheader('Data Management')
-    st.write(nettoyer_standardiser_donnees())
+with tabs_1:
+    st.subheader('Statistiques descriptives')
+    st.write('Remplacer par votre code')
     fig = correlation.map_corr(donnees)
     st.pyplot(fig)
 
 
-elif choix_etape == 'Split des données':
-    st.subheader('Split des données')
-    st.write(fractionner_donnees())
+with tabs_2:
+    st.subheader('Nettoyage des données')
+    st.write('Remplacer par votre code')
 
-elif choix_etape == 'Modèle':
+with tabs_3:
+    st.subheader('Préparation au modèle')
+    st.write('Remplacer par votre code')
+
+
+with tabs_4:
     st.subheader('Modèle')
-    st.write(entrainer_modele())
-
-
-elif choix_etape == 'Résultats':
-    st.subheader('Résultats')
-    st.write(evaluer_modele())
+    st.write('Remplacer par votre code')
+    
+with tabs_5:
+    st.subheader('Prédictions')
+    st.write('Remplacer par votre code')
 
 
 
