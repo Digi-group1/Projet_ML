ebe30df21a63eee3c9167b57b8e19221ac5330ff
AmandineAndre <aa.amandine.andre@gmail.com>
2024-02-19 14:28:13 +0100

    mise à jour fonctions

--- data_management/chargement_data.py
+++ data_management/chargement_data.py
@@ -1,7 +1,23 @@
 import pandas as pd 
+import streamlit as st
 
 def charger(nom_base):
     if nom_base == 'Diabète':
-        return pd.read_csv(r'data\diabete.csv', sep=",")
+        df = pd.read_csv(r'data\diabete.csv', sep=",")
+        base = 'Diabète'
     elif nom_base == 'Vins':
-        return pd.read_csv(r'data\vin.csv', sep=",")
+        df = pd.read_csv(r'data\vin.csv', sep=",")
+        base = 'Vins'
+    else:
+        # Charger un fichier CSV personnalisé
+        uploaded_file = st.sidebar.file_uploader("Charger un fichier CSV", type=["csv"])
+        separateur_csv = st.sidebar.text_input("Quel est le délimiteur de votre fichier")
+        
+        if uploaded_file is not None:
+            # Lire le fichier CSV chargé
+            df = pd.read_csv(uploaded_file, sep = separateur_csv)
+            base = st.sidebar.text_input("Nommez votre base de données", "MaBaseDeDonnees")
+        else:
+            # Afficher un message si aucun fichier n'a été chargé
+            st.warning("Veuillez charger un fichier CSV")
+    return df, base
--- data_management/nettoyage.py
+++ data_management/nettoyage.py
@@ -1,3 +1,6 @@
+import streamlit as st
+
+
 def colonnes_unnamed(data) :
     colonnes_non_nommees = [colonne for colonne in data.columns if colonne.startswith("Unnamed")]
     return colonnes_non_nommees
@@ -25,10 +28,101 @@ def identification_nan(data) :
     valeurs = data.isna().sum()
     if valeurs.sum() == 0 :
         message = 'Votre base de données ne contient pas de valeurs manquantes'
+        valeurs_null = "Non"
     else :
         message = 'Votre base de données contient des valeurs manquantes'
-    return message
+        valeurs_nan = valeurs[valeurs >0]
+        st.write(valeurs_nan)
+        valeurs_null = "Oui"
+    return message, valeurs_null
+
         
+def supprimer_colonnes(data) :
+    valeurs = data.isna().sum()
+    valeurs_nan = valeurs[valeurs >0]
+    colonnes_avec_nan = valeurs_nan.index.tolist()
+    data = data.drop(columns=colonnes_avec_nan,axis=1)
+    return data
+
+def remplacer_valeurs_manquantes(data):
+    colonnes_numeriques = data.select_dtypes(include=['float', 'int']).columns
+    colonnes_catégorielles = data.select_dtypes(include=['object']).columns
+    st.write("Pour remplacer les données nulles, nous appliquons deux méthodes : ")
+    st.write(" - Colonnes numériques : on remplace la valeur null par la moyenne de la colonne")
+    st.write(" - Colonnes catégorielles : on remplace la valeur null par la valeur de la colonne la plus fréquente")
+    
+    # Remplacer les valeurs manquantes dans les colonnes numériques par la moyenne
+    for colonne in colonnes_numeriques:
+        moyenne = data[colonne].mean()
+        data[colonne].fillna(moyenne, inplace=True)
+    
+    # Remplacer les valeurs manquantes dans les colonnes catégorielles par la valeur la plus fréquente
+    for colonne in colonnes_catégorielles:
+        valeur_frequente = data[colonne].mode()[0]
+        data[colonne].fillna(valeur_frequente, inplace=True)
+
+    return data
+
+
+def choix_traitement_nan(data) :
+    st.write('Que souhaitez-vous faire ?')
+    supprimer_lignes = st.checkbox("Supprimer toutes les lignes contenant des valeurs null ?")
+    traitement_commun = st.checkbox("Traiter toutes les colonnes de la même façon")
+    traitement_indiv = st.checkbox("Traiter les colonnes une par une")
+    
+    if supprimer_lignes :
+        st.write("<span style='color:red'>!! Attention, cette action peut fortement réduire le nombre d'enregistrements de votre base de données !! </span>", unsafe_allow_html=True)
+        nb_enreg = data.shape[0]
+        st.write("Votre base de données contient actuellement " + str(nb_enreg) + " enregistrements")
+        data_sans_nulles = data.dropna()
+        nb_enreg_apres = data_sans_nulles.shape[0]
+        nb_supp = nb_enreg - nb_enreg_apres
+        st.write("Vous allez supprimer " + str(nb_supp) + " enregistrements")
+        verification_supp = st.radio("Souhaitez-vous vraiment supprimer tous les enregistrements ?", ("Oui", "Non"))
+        if verification_supp == "Non" :
+            st.write("Veuillez sélectionner un autre choix au menu du dessus")
+        elif verification_supp == "Oui" :
+            data = data_sans_nulles
+            st.write(data.head())
+        
+
+    if traitement_commun :
+        choix_commun = st.radio("Quelle action souhaitez-vous effectuer ?", ("Supprimer les colonnes", "Remplacer les valeurs manquantes"))
+        if choix_commun == "Supprimer les colonnes" :
+            data = supprimer_colonnes(data)  
+            st.write(data.head()) 
+        if choix_commun == "Remplacer les valeurs manquantes" : 
+            data = remplacer_valeurs_manquantes(data)
+            st.write(data.head())
+    
+    if traitement_indiv :
+        valeurs = data.isna().sum()
+        valeurs_nan = valeurs[valeurs >0]
+        colonnes_avec_nan = valeurs_nan.index.tolist()
+        for colonne in colonnes_avec_nan :
+            choix_indiv = st.radio("Quelle action souhaitez-vous effectuer pour la colonne " + colonne + " ?", ("Supprimer la colonne", "Remplacer les valeurs manquantes"))
+            if choix_indiv == "Supprimer la colonne" :
+                data = data.drop(columns=colonne,axis=1)
+                st.write("Vous venez de supprimer la colonne "+ colonne + ". Voici votre nouvelle base de données")
+                st.write(data.head())
+            if choix_indiv == "Remplacer les valeurs manquantes" :
+                choix_remplace = st.radio("Par quelle valeur souhaitez-vous remplacer ?",("Une valeur unique","La moyenne/la valeur la plus fréquente"))
+                if choix_remplace == "Une valeur unique" :
+                    pass
+                if choix_remplace == "La moyenne/la valeur la plus fréquente" : 
+                    pass
+        
+    
+    return data
+            
+            
+        
+
+
+
+
+    
+
         
 
 
