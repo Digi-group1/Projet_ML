d8400f8d2765e7dae823088fc2aeaec17be24e0b
Digi-group1 <159011143+Digi-group1@users.noreply.github.com>
2024-02-20 14:09:28 +0100

    Update tab_4.py
    retouche appel fonction

--- tab_4.py
+++ tab_4.py
@@ -6,6 +6,6 @@ def etapes_model(features,target,type_model):
     if type_model == "regression":
         model = tab_4_regression.etapes(features,target)
     else:
-        model = tab_4_classification.etapes(features,target)
+        model = tab_4_classification.etapes_clf(features,target)
 
     return model
